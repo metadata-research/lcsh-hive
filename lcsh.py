e8bdf2ca5337044387f70984759d0c18e8845580
Chris R <cr625@drexel.edu>
2021-08-11 15:11:27 -0400

    updated to latest from live

--- lcsh.py
+++ lcsh.py
@@ -9,7 +9,7 @@ app = Flask(__name__)
 def index():
     return '<html>Welcome to the 1910 Library of Congress Subject Headings. Presented by HIVE2 start <a href = "/ark:/99152/b47p8tc5z">here</a> </html>'
 
-@app.route(r'/ark:/99152/<ark>')
+@app.route('/ark:/99152/<ark>')
 def get_term(ark):
     hive_uri = 'https://hive2.cci.drexel.edu/getConceptInfo?id={}&voc=lcsh1910'.format(ark)
     hive_response = requests.get(hive_uri,verify=False).json()
